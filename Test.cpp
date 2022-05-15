3f82de58bc9c4d5ddfca601ccd99d29b928ee36c
barg991 <barg991@gmail.com>
2022-05-15 21:08:22 +0300

    100

--- Test.cpp
+++ Test.cpp
@@ -1,11 +1,13 @@
 #include "doctest.h"
 #include <iostream>
 #include <algorithm>
-
-
+#include "/home/bar/Desktop/dev/CPP_Course/Assignment5/EX5_A_CPP/sources/OrgChart.hpp"
+using namespace ariel;
 using namespace std;
 TEST_CASE("GEE"){
+    OrgChart e{};
+    e.add_root("CEO");
     for(size_t i=0;i<20;i++){
-        CHECK(1==1);
+        CHECK_NOTHROW(e.begin_level_order());
     }
 }
